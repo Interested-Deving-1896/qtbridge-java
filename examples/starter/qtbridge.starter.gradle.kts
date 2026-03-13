e6392dbc55016e0092cdc3759afc40495cc019a3
Juha Vuolle <juha.vuolle@qt.io>
2026-03-13 12:42:01 +0200

    Bump plugin to 0.2-rc1
    As in: 0.2 milestone's first release candidate
    
    Change-Id: Ibbda5be3421ad1bd928cfa1b517d6de02a2c9c6d
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>

--- examples/starter/qtbridge.starter.gradle.kts
+++ examples/starter/qtbridge.starter.gradle.kts
@@ -15,7 +15,7 @@ data class ProjectConfig(
     val projectName: String,
     val packageName: String,
     val language: String,
-    val qtBridgeVersion: String = "0.1.2"
+    val qtBridgeVersion: String = "0.2-rc1"
 )
 
 object TemplateProvider {
