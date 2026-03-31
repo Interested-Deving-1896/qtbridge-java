86d3dc8d5b96c9c55ace9347e963574914dee342
Ahmed El Khazari <ahmed.el.khazari@qt.io>
2026-03-31 15:10:23 +0300

    Combine hover extension into ext file
    Task-number: QTBRIDGES-140
    Change-Id: Iddbd56ad34a14f66a00c6461248f4bd984ed49bf
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>

--- ide/intellij/src/main/kotlin/org/qtproject/qt/qtbridge/ide/utils/ext.kt
+++ ide/intellij/src/main/kotlin/org/qtproject/qt/qtbridge/ide/utils/ext.kt
@@ -6,10 +6,14 @@
 package org.qtproject.qt.qtbridge.ide.utils
 
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.HtmlBuilder
 import com.intellij.openapi.util.text.HtmlChunk
 import com.intellij.openapi.util.text.StringUtil
-
+import com.intellij.ui.ColorUtil
+import com.intellij.util.ui.UIUtil
+import org.eclipse.lsp4j.MarkupContent
 
 internal fun Project.resolveQmlLsCommandLine(): GeneralCommandLine? {
     return QmlToolingInfoParser.parse(this)?.let { QmlToolCommand.QmlLs.buildCommandLine(it.toQmlLSConfig()) }
@@ -24,4 +28,25 @@ internal fun HtmlChunk.Element.withFont(font: String?): HtmlChunk.Element {
 internal fun String.formatAsHtml(): String {
     val escaped = StringUtil.escapeXmlEntities(this)
     return escaped.replace(QUOTED_REGEX) { "<b>${it.value}</b>" }
+}
+
+internal fun MarkupContent.hoverContent(): String {
+    val scheme = EditorColorsManager.getInstance().globalScheme
+    val editorFont = scheme.editorFontName
+
+    return HtmlBuilder()
+        .append(
+            HtmlChunk.div()
+                .style("padding: 4px 8px;")
+                .withFont(editorFont)
+                .children(
+                    HtmlChunk.span()
+                        .style("color: ${ColorUtil.toHtmlColor(UIUtil.getLabelForeground())};")
+                        .addRaw(formattedValue())
+                )
+        ).toString()
+}
+
+private fun MarkupContent.formattedValue(): String {
+    return (value ?: "").trim().formatAsHtml()
 }
